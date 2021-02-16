dbe81fd0af9299c2ddf247b586bca5abe7ed3139
Gondowan <soulspire.fc@gmail.com>
2021-02-16 17:40:14 +0100

    Navbar & HomePage creation

---
+++ src/components/Slider.js
@@ -0,0 +1,25 @@
+import { Carousel } from 'react-materialize';
+
+const Slider = () => {
+
+    return (
+        <div
+            className="s12 m10 l8 xl6 carousel"
+        >
+            <Carousel
+                carouselId="Carousel-2"
+                images={[
+                    'https://image.freepik.com/free-vector/inventory-management-isometric-illustration_1124-929.jpg',
+                    'https://image.freepik.com/free-vector/inventory-management-isometric-illustration_1124-929.jpg',
+                    'https://image.freepik.com/free-vector/inventory-management-isometric-illustration_1124-929.jpg'
+                ]}
+                options={{
+                    fullWidth: true,
+                    indicators: true
+                }}
+            />
+        </div>
+        )
+}
+
+export default Slider;
