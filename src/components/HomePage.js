b08eeb08f1507f696b61674ae4c248113c8e3fd6
patrickminero <58296867+mineropatricio@users.noreply.github.com>
2021-03-02 19:47:07 +0100

    add video background

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,18 +8,24 @@ import HomepageTabs from './HomepageTabs.js';
 const HomePage = () => {
 
     return (
+        <div>
+            <div className="home-title">
+                <video autoPlay muted loop id="myVideo">
+                    <source src="https://res.cloudinary.com/dliczkabg/video/upload/v1614708923/videos/production_ID_4281239_ftjn1g.mp4" type="video/mp4" />
+                </video>
+                <div className="heading">
+                    <h1>Take control of your business</h1>
+                    <h5>Visualize the way you manage inventory and ordering. </h5>
+                    <Link to="/contact" className="btn btn-large home-title-btn deep-orange lighten-1 hoverable">
+                        Learn more
+                        <Icon right>
+                        send
+                        </Icon>
+                    </Link>
+                </div>
+            </div>
         <div className="App">
-         <div className="home-title">
-            <h1>Take control of your business</h1>
-            <h5>Visualize the way you manage inventory and ordering. </h5>
-            <Link to="/contact" className="btn btn-large home-title-btn deep-orange lighten-1 hoverable">
-                Learn more
-                <Icon right>
-                send
-                </Icon>
-            </Link>
-         </div>
-            <Slider/>
+            {/* <Slider/> */}
             <HomeCard/>
             <div className="home-card">
             <h1>Easy to access inventory dashboard</h1>
@@ -40,6 +46,7 @@ const HomePage = () => {
                 </Button>
             </CardPanel>
         </div>
+    </div>
     )
 }
 
